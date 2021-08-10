db61e56c61db2dd43432eaac760a4a5ab1abd467
bygirish <girishkumargupta3@gmail.com>
2021-08-10 19:31:37 +0530

    Added FB and Ln Component UI

---
+++ app/presentation/auth/SocialAuth/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SocialAuthFB = () => {
+
+    return (
+        <div>
+            {'FB'}
+        </div>
+    )
+}
+
+const SocialAuthLn = () => {
+
+    return (
+        <div>
+            {'Ln'}
+        </div>
+    )
+}
+
+export {
+    SocialAuthFB,
+    SocialAuthLn
+}
